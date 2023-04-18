0d5ac2714b88e84eb3f3e36887aa4cbd5fe226fc
José Arthur De Siqueira Vieira <105373719+Morkgud@users.noreply.github.com>
2023-04-18 01:07:33 -0300

    Delete Exercicio8.py

--- Exercicio8.py
+++
@@ -1,7 +0,0 @@
-n = float(input("insira o valor em metros: "))
-
-conversao1 = n*100
-conversao2 = n*1000
-
-print("o valor em metros convertido para centimetros equivale {} centimetros e em milimetros equivale {} milimetros".format(conversao1,conversao2))
-
