0d6d2cf90a3069736d340e6204c727e112ebe837
José Arthur De Siqueira Vieira <105373719+Morkgud@users.noreply.github.com>
2023-04-18 01:08:33 -0300

    Delete Exercicio6.py

--- Exercicio6.py
+++
@@ -1,7 +0,0 @@
-n = int(input("digite um valor: "))
-
-dobro = n*2
-triplo = n*3
-raiz = n**0.5
-
-print("o dobro do número é {}, o triplo é {} e a raiz quadrada equivale a {}".format(dobro,triplo,raiz))
